a4152f622915bc6bdb82d6e3f37d59a0aa638fbd
Filip <szymanski.fp@gmail.com>
2021-09-22 21:47:19 +0200

    controller working

--- src/main/java/eu/filip/springrestapiusers/UserController.java
+++ src/main/java/eu/filip/springrestapiusers/UserController.java
@@ -1,15 +1,19 @@
 package eu.filip.springrestapiusers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserController {
+
 
     @GetMapping("/user")
     public User user(@RequestParam(value="name", defaultValue = "unknown") String name){
         return new User(1, name, "last" + name);
     }
+    
+
 }
