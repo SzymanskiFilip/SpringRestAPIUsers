c3babdcf8e93617c45246c413c4cc745b6f7c3a7
Filip <szymanski.fp@gmail.com>
2021-09-22 20:54:37 +0200

    created User class

---
+++ src/main/java/eu/filip/springrestapiusers/User.java
@@ -0,0 +1,34 @@
+package eu.filip.springrestapiusers;
+
+public class User {
+    private final long id;
+
+    private String name;
+    private String lastName;
+
+    public User(long id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
