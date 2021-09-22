79409248390ab1bdfe6306511ba71147677ff1e4
Filip <szymanski.fp@gmail.com>
2021-09-22 21:55:44 +0200

    Json reader working

--- src/main/java/eu/filip/springrestapiusers/JSONReader.java
+++ src/main/java/eu/filip/springrestapiusers/JSONReader.java
@@ -8,5 +8,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class JSONReader {
-
+    public static List<User> readUsersFromFile() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(Paths.get("users.json").toFile(), mapper.getTypeFactory().constructParametricType(List.class, Object.class));
+    }
 }
