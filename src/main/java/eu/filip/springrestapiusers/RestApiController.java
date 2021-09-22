563386847dc0046a434663d0917e51c2ef2d6020
Filip <szymanski.fp@gmail.com>
2021-09-22 20:48:20 +0200

    added test controller

--- src/main/java/eu/filip/springrestapiusers/RestApiController.java
+++ src/main/java/eu/filip/springrestapiusers/RestApiController.java
@@ -1,2 +1,14 @@
-package eu.filip.springrestapiusers;public class RestApiController {
+package eu.filip.springrestapiusers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+    @RequestMapping("/test")
+    String testTEXT(){
+        return "HELLO WORLD!";
+    }
 }
