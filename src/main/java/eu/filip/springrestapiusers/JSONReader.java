6d09967da9f210bb6d0719e08f9f19f50add3b90
Filip <szymanski.fp@gmail.com>
2021-09-22 21:47:08 +0200

    created JSONReader class

---
+++ src/main/java/eu/filip/springrestapiusers/JSONReader.java
@@ -0,0 +1,12 @@
+package eu.filip.springrestapiusers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class JSONReader {
+
+}
