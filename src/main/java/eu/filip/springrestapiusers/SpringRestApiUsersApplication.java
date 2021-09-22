b716dd85e6fd3e597bdb328a9edf8a9c1b4e0f13
Filip <szymanski.fp@gmail.com>
2021-09-22 20:42:14 +0200

    initialized project

---
+++ src/main/java/eu/filip/springrestapiusers/SpringRestApiUsersApplication.java
@@ -0,0 +1,13 @@
+package eu.filip.springrestapiusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApiUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/filip/springrestapiusers/SpringRestApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.filip.springrestapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
