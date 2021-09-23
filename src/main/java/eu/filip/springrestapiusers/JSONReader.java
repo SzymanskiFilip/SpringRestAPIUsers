ec647df432034a2d48939d1115268d9341bdd318
Filip <szymanski.fp@gmail.com>
2021-09-23 21:53:09 +0200

    getUserFrom id working

--- src/main/java/eu/filip/springrestapiusers/JSONReader.java
+++ src/main/java/eu/filip/springrestapiusers/JSONReader.java
@@ -1,9 +1,11 @@
 package eu.filip.springrestapiusers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,4 +14,20 @@ public class JSONReader {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(Paths.get("users.json").toFile(), mapper.getTypeFactory().constructParametricType(List.class, Object.class));
     }
+
+    public static User getUserFromId(int id) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<User> users = JSONReader.readUsersFromFile();
+        List<User> list = mapper.convertValue(users, new TypeReference<List<User>>() {});
+        if(id > users.size()){
+            return new User(0, "NO USER WITH THAT ID", "NO USER WITH THAT ID");
+        }
+        try{
+            return list.get(id - 1);
+        } catch (IndexOutOfBoundsException e){
+            e.printStackTrace();
+            return new User(0, "NO USER WITH THAT ID", "NO USER WITH THAT ID");
+        }
+    }
+
 }
