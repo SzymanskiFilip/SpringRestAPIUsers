17bb703bd4ea51b0aabcf5be602bc9fae0b1f6e7
Filip <szymanski.fp@gmail.com>
2021-09-23 21:52:43 +0200

    /user/{id} endpoint that returns the user with the given id

--- src/main/java/eu/filip/springrestapiusers/UserController.java
+++ src/main/java/eu/filip/springrestapiusers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}")
-    public int user(@PathVariable Long id) throws IOException {
-        return id.intValue();
+    public User user(@PathVariable Long id) throws IOException {
+        return JSONReader.getUserFromId(id.intValue());
     }
 }
